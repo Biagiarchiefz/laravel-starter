c6b1a4b028d146dd5ee846af19e2ff1e1db19f99
Biagiarchiefz <biagiiarchie@gmail.com>
2025-09-07 14:08:14 +0700

    Database & Migration

---
+++ app/Models/Post.php
@@ -0,0 +1,48 @@
+<?php
+
+//ini untuk memberitau laravelnya kalau Post berada di folder App Models
+namespace App\Models;
+use Illuminate\Support\Arr;
+
+class Post {
+
+    public static function all() {
+        return [
+            [
+                'id' => 1,
+                'slug' => 'membuat-website-hanya-5menit-menggunakan-html',
+                'title' => 'Membuat Website Hanya 5 Menit Menggunakan Html',
+                'author' => 'Biagi Archie Fais',
+                'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae earum, eligendi facilis fugit laudantium nesciunt nulla omnis pariatur, perferendis quasi quibusdam repudiandae, tempore voluptates! Ducimus!'
+            ],
+            [
+                'id' => 2,
+                'slug' => 'styling-dengan-mudah-menggunakan-tailwind',
+                'title' => 'Styling Dengan Mudah Menggunakan Tailwind',
+                'author' => 'Faiz Jadul',
+                'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae earum, eligendi facilis fugit laudantium nesciunt nulla omnis pariatur, perferendis quasi quibusdam repudiandae, tempore voluptates! Ducimus!'
+            ],
+        ];
+    }
+
+    // : array merupakan type return dari function ini berupa array agar lebih spesifik
+    public static function find($slug): array {
+
+        // memanggil method lain di class yang sama kita menggunakan static
+//        return Arr::first(static::all(), function ($post) use ($slug) {
+//            return $post['slug'] === $slug;   // kalaiu slug dari data Posts sama dengan slug yang dari URL return element datanya
+//        });
+
+        $post = Arr::first(static::all(), fn($post) => $post['slug'] === $slug); // contoh yang mennggunakan arrow function php tanpa menggunakna use
+
+        if ( !$post ) {
+            abort(404, 'Post not found.');
+        }
+
+        return $post;
+
+    }
+
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
+//halaman route ini digunakna sebagain penjaluran dari request mirip seperti react router yang biasanya di file app.jsx
 
-use Illuminate\Support\Arr;
+
+
 use Illuminate\Support\Facades\Route;
+
+//panggil class Post yang ada di folder App Models yang sudah kita daftarkan dari Folder App Models
+use App\Models\Post;
+
+
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);    // menuju file views dengan nama home.blade.php
@@ -14,52 +21,20 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => [
-        [
-            'id' => 1,
-            'slug' => 'membuat-website-hanya-5menit-menggunakan-html',
-            'title' => 'Membuat Website Hanya 5 Menit Menggunakan Html',
-            'author' => 'Biagi Archie Fais',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae earum, eligendi facilis fugit laudantium nesciunt nulla omnis pariatur, perferendis quasi quibusdam repudiandae, tempore voluptates! Ducimus!'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'styling-dengan-mudah-menggunakan-tailwind',
-            'title' => 'Styling Dengan Mudah Menggunakan Tailwind',
-            'author' => 'Faiz Jadul',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae earum, eligendi facilis fugit laudantium nesciunt nulla omnis pariatur, perferendis quasi quibusdam repudiandae, tempore voluptates! Ducimus!'
-        ],
-    ]]);
+    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
 });
 
 
 
 //parameter kedua dari get() merupakan callback function yang akan dijalankan ketikan kita mengakses urlnya di parameter pertama
-// /posts/{slug}, {slug}nya disebute wilecard, sama seperti slug di react router seperti /posts/:slug
+// /posts/{slug}, {slug}nya disebute wilecard, sama seperti dynamic route di react router contoh seperti /posts/:slug
 // parameter di function callbacknya itu menangkap slug dari url
 Route::get('/posts/{slug}', function ($slug) {
 
-     $posts = [
-         [
-             'id' => 1,
-             'slug' => 'membuat-website-hanya-5menit-menggunakan-html',
-             'title' => 'Membuat Website Hanya 5 Menit Menggunakan Html',
-             'author' => 'Biagi Archie Fais',
-             'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae earum, eligendi facilis fugit laudantium nesciunt nulla omnis pariatur, perferendis quasi quibusdam repudiandae, tempore voluptates! Ducimus!'
-         ],
-         [
-             'id' => 2,
-             'slug' => 'styling-dengan-mudah-menggunakan-tailwind',
-             'title' => 'Styling Dengan Mudah Menggunakan Tailwind',
-             'author' => 'Faiz Jadul',
-             'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae earum, eligendi facilis fugit laudantium nesciunt nulla omnis pariatur, perferendis quasi quibusdam repudiandae, tempore voluptates! Ducimus!'
-         ],
-     ];
+
 
 //     first itu mencari element pertama di dalam array berdasarkan kriteria yang kita mau cari, miri seperti find() kalau di javaScript
-     $post = Arr::first($posts, function ($value) use ($slug) {
-         return $value['slug'] === $slug;
-     });
+     $post = Post::find($slug);
 
      return view('post', ['title' => 'Single Post','post' => $post]);
 
@@ -69,3 +44,4 @@ Route::get('/posts/{slug}', function ($slug) {
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
 });
+
